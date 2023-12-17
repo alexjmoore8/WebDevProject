986e3752f27bc4ebbb5da33ca7921b134ab02919
Alden Park <jackleekpop35@gmail.com>
2023-12-18 02:27:24 +0900

    homepage updates

--- src/components/HomeA.js
+++ src/components/HomeA.js
@@ -11,6 +11,14 @@ const Home = () => {
             <Navbar />
             <h1>Welcome to the Employer Homepage!</h1>
 
+            <div className="homepage-bio">
+                <p>Welcome to our beta stage web application, designed to connect you with your dream job. 
+                   For job seekers, we streamline the resume creation process, breaking it down into manageable 
+                   steps and guiding you on what information is essential. For employers, we offer access to 
+                   a curated selection of top talent. Our goal is to make the job matching process as efficient 
+                   and effective as possible for both parties.</p>
+            </div>
+
             <div className="home-buttons">
                 {/* Conditional rendering based on the user role */}
                 {auth.role === 'applicant' && (
