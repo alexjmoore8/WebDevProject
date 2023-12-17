fa8b0e6af24b9e3ec5d09fdeb58bd18973d3d1ce
Alex Moore <105068100+alexjmoore8@users.noreply.github.com>
2023-12-17 00:32:48 -0600

    updated job post list

--- src/components/jobPost/jobPostList.js
+++ src/components/jobPost/jobPostList.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import './css/jobPost.css'; // Import your CSS file for styles
 
 export function JobPostList() {
     const navigate = useNavigate();
     const [jobs, setJobs] = useState([]);
-    const [message, setMessage] = useState('');
+    const [expandedJobId, setExpandedJobId] = useState(null);
 
     useEffect(() => {
         getJobs();
@@ -14,32 +15,48 @@ export function JobPostList() {
     async function getJobs() {
         try {
             const response = await axios.get("http://localhost:3000/jobs", { withCredentials: true });
-            console.log(response.data);
             setJobs(response.data);
         } catch (error) {
-            setMessage("Error during login");
+            console.error("Error fetching job posts", error);
+        }
+    }
+
+    function handleJobClick(jobId) {
+        if (expandedJobId === jobId) {
+            setExpandedJobId(null);
+        } else {
+            setExpandedJobId(jobId);
         }
     }
 
     function handleBackClick() {
-        navigate(-1); 
+        navigate(-1);
     }
 
     return (
         <div className='jobList'>
             <h1>Available Job Listings</h1>
             <ul>
-                {jobs.map((job, i) => (
-                    <li key={i}>
+                {jobs.map((job) => (
+                    <li key={job._id} className="jobItem" onClick={() => handleJobClick(job._id)}>
                         <div>
+                          <div className="jobListItem">
+                          <div className={`caret ${expandedJobId === job._id ? 'expanded' : ''}`}>&#9660;</div>
                             <h2>{job.title} - {job.companyName}</h2>
-                            <p>{job.description}</p>
-                            <p>{job.requirements}</p>
+                          </div>
+                            {expandedJobId === job._id && (
+                                <div>
+                                    <p>Description: {job.description}</p>
+                                    <p>City: {job.city}</p>
+                                    <p>State: {job.state}</p>
+                                    <p>Salary: {job.salary}</p>
+                                    <p>Tags: {job.tags.join(', ')}</p>
+                                </div>
+                            )}
                         </div>
                     </li>
                 ))}
             </ul>
-            {message && <p className={`message error-message`}>{message}</p>}
             <button onClick={handleBackClick} className="back-button">Back</button>
         </div>
     );
